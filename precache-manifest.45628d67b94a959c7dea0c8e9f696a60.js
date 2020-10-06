5f7f5d4b4693c8b5e3fcadf1160395cfb6a4ec3a
Neal Patel (nealdotpy) <nealpatel2021@gmail.com>
2020-10-06 11:47:35 -0400

    Updates

---
+++ precache-manifest.45628d67b94a959c7dea0c8e9f696a60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fd9486d0d3395b7de39238aa4e2533f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d63606c1ef73a6b8c63b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "09dbc9173a8f8a2448f4",
+    "url": "/static/js/2.0688f8fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0688f8fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d63606c1ef73a6b8c63b",
+    "url": "/static/js/main.f7a71947.chunk.js"
+  },
+  {
+    "revision": "ac51998dac849531492e",
+    "url": "/static/js/runtime-main.baa6f425.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
