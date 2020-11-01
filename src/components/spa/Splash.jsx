5f67bfa2aa334d0527d4c79e826df4f86439914e
nealdotpy <nealpatel2021@gmail.com>
2020-11-01 14:40:10 -0500

    props for blinking and spaced cursor.

--- src/components/spa/Splash.jsx
+++ src/components/spa/Splash.jsx
@@ -4,13 +4,15 @@ import ParticleBackground from "./ParticleBackground";
 
 // className="title jumbotron jumbotron-fluid bg-transparent bgstyle text-light min-vh-100 d-flex align-content-center align-items-center flex-wrap m-0"
 
+const cursorProps = {blink: true};
+
 const Splash = () => {
   return (
     <div className="splash">
       <ParticleBackground/>
       <div className="splash-text">
-        <Typist>
-          The Invisible Impact
+        <Typist cursor={cursorProps}>
+          {"The Invisible Impact "}
          </Typist>
       </div>
     </div>
