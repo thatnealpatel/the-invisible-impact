1cfc16bfc89a3b46176caa48d76a643c5a8074ed
nealdotpy <nealpatel2021@gmail.com>
2020-11-03 08:47:25 -0500

    refactor line length.

--- src/components/Qr.jsx
+++ src/components/Qr.jsx
@@ -3,9 +3,14 @@ import ParticleBackground from "./spa/ParticleBackground";
 import { hotjar } from 'react-hotjar';
 
 const prompts = {
-  1: "Who should bear the greatest responsibility in repairing ecosystems and lives damaged by environmental problems? [1]",
-  2: "How do you see yourself becoming involved in environmental justice beyond acts of consumption (e.g. buying 'sustainable' products)? [2]",
+  1: "Who should bear the greatest responsibility in repairing ecosystems and lives damaged \
+      by environmental problems? [1]",
+
+  2: "How do you see yourself becoming involved in environmental justice beyond acts of \
+      consumption (e.g. buying 'sustainable' products)? [2]",
+
   3: "What frustrates you about environmental justice? [3]",
+  
   4: "What should our top environmental concern be? [4]",
 };
 
