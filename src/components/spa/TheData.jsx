de621d719aaa023fa0335c7a28d00e3498e5fcbc
nealdotpy <nealpatel2021@gmail.com>
2020-11-12 18:30:45 -0500

    skeleton for data vis.

--- src/components/spa/TheData.jsx
+++ src/components/spa/TheData.jsx
@@ -1,34 +1,92 @@
 import React from "react";
 import Typist from "react-typist";
+import { Chart } from "react-charts";
+import responseSchema from "../../media/response_data.json";
 
 const cursorProps = {blink: true};
 
-class Gallery extends React.Component {
+function BarChart() {
+  const responseData = React.useMemo(
+    () => [
+     {
+       label: 'Series 1',
+       data: [
+         { primary: 1, secondary: 10 },
+         { primary: 2, secondary: 10 },
+         { primary: 3, secondary: 10 },
+       ],
+     },
+     {
+       label: 'Series 2',
+       data: [
+         { primary: 1, secondary: 10 },
+         { primary: 2, secondary: 10 },
+         { primary: 3, secondary: 10 },
+       ],
+     },
+     {
+       label: 'Series 3',
+       data: [
+         { primary: 1, secondary: 10 },
+         { primary: 2, secondary: 10 },
+         { primary: 3, secondary: 10 },
+       ],
+     },
+    ],
+    []
+  );
 
-  importAll(context) {
-    return context.keys().map(context);
-  }
+  const chartAxes = React.useMemo(
+    () => [
+      { primary: true, type: 'ordinal', position: 'left' },
+      { stacked: true, type: 'linear', position: 'bottom' },
+    ],
+    []
+  );
 
-  componentWillMount() {
-    
-  }
+  const chartSeries = React.useMemo(
+    () => ({
+      type: "bar"
+    }),
+    []
+  );
 
-  render() {
-    return (
-      <section id="thedata" className="thedata section">
-        <div id="thedata-content">
-          <div className="thedata-header">
-            <Typist cursor={cursorProps}>
-              {"The Data "}
-             </Typist>
-          </div>
-          <div className="thedata-container">
-            
-          </div>
-         </div>
-      </section>
-    );
-  }
+  return (
+    <section id="thedata" className="thedata section">
+      <div id="thedata-content">
+        <div className="thedata-header">
+          <Typist cursor={cursorProps}>
+            {"The Data "}
+           </Typist>
+        </div>
+        <div className="thedata-container">
+          <Chart data={responseData} axes={chartAxes} series={chartSeries} />
+        </div>
+       </div>
+    </section>
+  )
+
 }
 
-export default Gallery;
+
+const TheData = () => {
+
+
+
+  return (
+    <section id="thedata" className="thedata section">
+      <div id="thedata-content">
+        <div className="thedata-header">
+          <Typist cursor={cursorProps}>
+            {"The Data "}
+           </Typist>
+        </div>
+        <div className="thedata-container">
+
+        </div>
+       </div>
+    </section>
+  );
+};
+
+export default BarChart;
